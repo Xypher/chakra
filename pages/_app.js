55ce59c8eb647d2561575a8aa7b9a98e2ad6c489
ammar <Ammar.Abu.Yaman@gmail.com>
2021-08-24 01:38:38 +0300

    initial commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,22 @@
+import { ColorModeProvider, ThemeProvider, Flex, CSSReset } from '@chakra-ui/react'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider>
+        <ColorModeProvider>
+          <Flex direction='columns' align='center' justify='center'>
+            <CSSReset/>
+            {/*<NavBar/>*/}
+            <Flex justify='center' align='center' w='100%' h='93vh'>
+
+              <Component {...pageProps} />  
+              
+            </Flex>
+          </Flex>
+        </ColorModeProvider>
+    </ThemeProvider>
+    )
 }
 
 export default MyApp
