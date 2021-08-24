5a0e46247177f886eda3b3996c6e160ef770c132
JU-Tooth-analysis-Project <juteethdesignproject@gmail.com>
2021-08-24 20:34:34 +0300

    commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,22 +1,27 @@
-import { ColorModeProvider, ThemeProvider, Flex, CSSReset } from '@chakra-ui/react'
-import '../styles/globals.css'
+import {
+  ChakraProvider,
+  ColorModeProvider,
+  ThemeProvider,
+  Flex,
+  CSSReset,
+} from "@chakra-ui/react";
+import "../styles/globals.css";
+import { extendTheme, theme } from "@chakra-ui/react";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ThemeProvider>
-        <ColorModeProvider>
-          <Flex direction='columns' align='center' justify='center'>
-            <CSSReset/>
-            {/*<NavBar/>*/}
-            <Flex justify='center' align='center' w='100%' h='93vh'>
-
-              <Component {...pageProps} />  
-              
-            </Flex>
+    <ChakraProvider>
+      <ThemeProvider theme={theme}>
+        <Flex direction="columns" align="center" justify="center">
+          <CSSReset />
+          {/*<NavBar/>*/}
+          <Flex justify="center" align="center" w="50%" h="93vh">
+            <Component {...pageProps} />
           </Flex>
-        </ColorModeProvider>
-    </ThemeProvider>
-    )
+        </Flex>
+      </ThemeProvider>
+    </ChakraProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,63 @@
-import { Box } from "@chakra-ui/react";
+import {
+  Button,
+  Divider,
+  FormControl,
+  FormHelperText,
+  Icon,
+  Input,
+  InputGroup,
+  InputLeftAddon,
+  Stack,
+} from "@chakra-ui/react";
 
 export default function index() {
   return (
-    <Box w='200px' h='15vh' bg='gray.200'/>
-  )
+    <form action="submit">
+      <Stack spacing={3}>
+        <FormControl isRequired>
+          <InputGroup>
+            {/*<InputLeftElement children={<Icon name="info" />} />*/}
+            <Input
+              type="name"
+              placeholder="First name"
+              aria-label="First name"
+            />
+          </InputGroup>
+        </FormControl>
+
+        <FormControl isRequired>
+          <InputGroup>
+            {/*<InputLeftElement children={<Icon name="info" />} />*/}
+            <Input type="name" placeholder="Last name" aria-label="Last name" />
+          </InputGroup>
+        </FormControl>
+
+        <Divider />
+
+        <FormControl isRequired>
+          <InputGroup>
+            {/*<InputLeftElement children={<Icon name="email" />} />*/}
+            <Input type="email" placeholder="Email" aria-label="Email" />
+          </InputGroup>
+        </FormControl>
+
+        <FormControl isRequired>
+          <InputGroup>
+            {/*<InputLeftElement children={<Icon name="lock" />} />*/}
+            <Input
+              type="password"
+              placeholder="Password"
+              aria-label="Password"
+            />
+          </InputGroup>
+        </FormControl>
+
+        <Button type="submit">Sign up</Button>
+        <FormHelperText>
+          hi ThemeProvider
+          <br />
+        </FormHelperText>
+      </Stack>
+    </form>
+  );
 }
